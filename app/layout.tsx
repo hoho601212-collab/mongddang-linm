54d7f69fd2cc68985c6a680380fbfd39f1414906
hoho601212-collab <hoho601212@gmail.com>
2026-09-02 12:11:02 +0900

    Import SEO styles globally from root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import Link from 'next/link';
 import './globals.css';
+import './seo.css';
 import { navigation, siteConfig } from '@/lib/site-config';
 
 export const metadata: Metadata = {
