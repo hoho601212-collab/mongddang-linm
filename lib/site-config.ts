f7b497652dcf2a658d487e966a876184a62acaa0
hoho601212-collab <hoho601212@gmail.com>
2026-09-02 11:45:12 +0900

    Add shared site and contact config

---
+++ lib/site-config.ts
@@ -0,0 +1,25 @@
+export const siteConfig = {
+  name: '몽땅 다이아',
+  brand: '몽땅 다이아',
+  mainKeyword: '린M',
+  phone: '010-6289-3810',
+  phoneHref: 'tel:01062893810',
+  kakaoId: 'pay3810',
+  kakaoUrl: 'http://qr.kakao.com/talk/i2upgpbu1am6frQeVqvvHamKNWg-',
+  domain: 'https://example.com',
+  description: '린M 다이아, 충전, 아이템, 패키지와 할인 정보를 안내하는 몽땅 다이아입니다.'
+} as const;
+
+export const navigation = [
+  { label: '린M충전', href: '/린M충전' },
+  { label: '린M다이아', href: '/린M다이아' },
+  { label: '린M아이템', href: '/린M아이템' },
+  { label: '린M패키지', href: '/린M패키지' },
+  { label: '린M가격', href: '/린M가격' },
+  { label: '린M충전방법', href: '/린M충전방법' },
+  { label: '린M할인', href: '/린M할인' },
+  { label: 'FAQ', href: '/린M자주묻는질문' },
+  { label: '린M이벤트', href: '/린M이벤트' },
+  { label: '린M업데이트', href: '/린M업데이트' },
+  { label: '린M가이드', href: '/린M가이드' }
+] as const;
